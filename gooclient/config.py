9556ef353cd92200d9ae1dc084b877af06c332d0
Gabriel A. von Winckler <winckler@ncc.unesp.br>
2013-05-23 17:26:24 -0300

    Create a template config file if missing

--- gooclient/config.py
+++ gooclient/config.py
@@ -1,30 +1,23 @@
-from ConfigParser import ConfigParser
+import ConfigParser
 import os
 import sys
 import requests
 import getpass
 
 class GooConfig():
-    gooconfig = None
     config = "~/.goorc"
 
-    debug = False
-    api_uri = None
-
-    # Only used when no token is valid
-    username = None
-    password = None
-
     def __init__(self):
-        self.gooconfig = ConfigParser()
+        self.gooconfig = ConfigParser.ConfigParser()
         try:
             self.gooconfig.readfp(open(os.path.expanduser(self.config)))
         except IOError:
-            print "Error: Could not open config file."
-            print "Create a %s file with 600 permissions." % self.config
-            print "Aborting..."
-            sys.exit()
-
+            print "Warning: Could not open config file."
+            print "Creating a new one..."
+            os.open(os.path.expanduser(self.config), os.O_WRONLY | os.O_CREAT, 0600)
+            self.gooconfig.add_section("global")
+            self.gooconfig.add_section("auth")
+            self.save_config()
 
         self.load_global()
 
@@ -32,21 +25,21 @@ class GooConfig():
         # Try to read global section
         try:
             self.debug = self.gooconfig.get("global", "debug")
-        except:
-            pass
+        except ConfigParser.NoOptionError:
+            self.debug = False
 
         try:
             self.api_uri = self.gooconfig.get("global", "api_uri")
-        except:
-            print "Error: Could not read api_uri."
-            print "Please check global section."
-            print "Aborting..."
-            sys.exit()
+        except ConfigParser.NoOptionError:
+            print "Warning: No server URL found."
+            self.api_uri = raw_input("URL: ")
+            self.gooconfig.set("global", "api_uri", self.api_uri)
+            self.save_config()
 
     def read_token(self):
         try:
             token = self.gooconfig.get("auth", "token")
-        except:
+        except ConfigParser.NoOptionError:
             return None
 
         return token
@@ -66,7 +59,9 @@ class GooConfig():
 
     def save_token(self, token):
         self.gooconfig.set("auth", "token", token)
+        self.save_config()
 
+    def save_config(self):
         f = open(os.path.expanduser(self.config), 'w')
         self.gooconfig.write(f)
         f.close()
